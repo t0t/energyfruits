629004b2a0cbf21d3779086361f0c02eb4904604
Sergio Forés <sergiofores@gmail.com>
2015-02-09 16:56:10 +0100

    init

--- functions.php
+++ functions.php
@@ -85,7 +85,7 @@ add_action( 'admin_menu' , 'quita_metaboxes_inutiles' );
 
 
 
-/**
+/*
  * Scripts and stylesheets
  *
  * Enqueue stylesheets in the following order:
@@ -93,68 +93,85 @@ add_action( 'admin_menu' , 'quita_metaboxes_inutiles' );
  *
  * Enqueue scripts in the following order:
  * 1. jquery-1.11.1.min.js via Google CDN
- * 2. /theme/assets/js/vendor/modernizr.min.js
  * 3. /theme/assets/js/scripts.js (in footer)
- *
- * Google Analytics is loaded after enqueued scripts if:
- * - An ID has been defined in config.php
- * - You're not logged in as an administrator
  */
-function roots_scripts() {
-  /**
-   * The build task in Grunt renames production assets with a hash
-   * Read the asset names from assets-manifest.json
-   */
 
-    $assets = array(
+function roots_scripts() {
+
+	$assets = array(
       'css'       => '/style.css',
       'js'        => '/assets/js/main.min.js',
       'jquery'    => '//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js'
     );
 
-  wp_enqueue_style('t0theme_css', get_template_directory_uri() . $assets['css'], false, null);
 
-  /**
-   * jQuery is loaded using the same method from HTML5 Boilerplate:
-   * Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
-   * It's kept in the header instead of footer to avoid conflicts with plugins.
-   */
-  if (!is_admin() && current_theme_supports('jquery-cdn')) {
-    wp_deregister_script('jquery');
-    wp_register_script('jquery', $assets['jquery'], array(), null, false);
-    add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
-  }
+    wp_enqueue_style(
 
-  wp_enqueue_script('jquery');
-  wp_enqueue_script('t0theme_js', get_template_directory_uri() . $assets['js'], array(), null, true);
+    	't0theme_css',
+    	 
+    	get_template_directory_uri() . $assets['css'], 
+
+    	false, 
+
+    	null
+
+    );
+
+
+    if (!is_admin() && current_theme_supports('jquery-cdn')) {
+
+    	wp_deregister_script('jquery');
+
+    	wp_register_script('jquery', $assets['jquery'], array(), null, false);
+
+    	add_filter('script_loader_src', 'roots_jquery_local_fallback', 10, 2);
+
+    }
+
+    wp_enqueue_script('jquery');
+
+
+    wp_enqueue_script(
+
+	  	't0theme_js',
+
+	  	get_template_directory_uri() . $assets['js'], 
+
+	  	array(), 
+
+	  	null, 
+
+	  	true
+
+  	);
+
 }
+
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
 
 
 
 // http://wordpress.stackexchange.com/a/12450
 function roots_jquery_local_fallback($src, $handle = null) {
+
   static $add_jquery_fallback = false;
 
   if ($add_jquery_fallback) {
+
     echo '<script>window.jQuery || document.write(\'<script src="' . get_template_directory_uri() . '/assets/vendor/jquery/dist/jquery.min.js?1.11.1"><\/script>\')</script>' . "\n";
     $add_jquery_fallback = false;
+
   }
 
   if ($handle === 'jquery') {
+
     $add_jquery_fallback = true;
+
   }
 
   return $src;
+
 }
+
 add_action('wp_head', 'roots_jquery_local_fallback');
 
-// encolar jquery de WP TODO: hacer que cargue en footer
-// function my_scripts_method() {
-// 	wp_enqueue_script(
-// 		'custom-script',
-// 		get_stylesheet_directory_uri() . 'assets/js/main.min.js',
-// 		array( 'jquery' )
-// 	);
-// }
-// add_action( 'wp_enqueue_scripts', 'my_scripts_method' );
