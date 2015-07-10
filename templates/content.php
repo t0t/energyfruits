bfa2ed9bd01f13f8c12a44ce2acc947e440f70ee
Sergio Forés <sergiofores@gmail.com>
2015-07-10 18:44:07 +0200

    Solving Bugs :beetle:

--- templates/content.php
+++ templates/content.php
@@ -1,15 +1,10 @@
-	
-	<!-- Page Header -->
-    <?php //get_template_part('templates/page', 'header'); ?>
+<!-- Loop -->
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+	<!-- Cargame los Custom fields, ACF -->
+	<?php get_template_part('templates/custom-fields'); ?>
 
-    <!-- Loop -->
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<?php endwhile; ?>
 
-	    <!-- Cargame los Custom fields, ACF -->
-	    <?php get_template_part('templates/custom-fields'); ?>
-
-    <?php endwhile; ?>
-
-    <?php else: ?>
-    <?php endif; ?>
+<?php else: ?>
+<?php endif; ?>
