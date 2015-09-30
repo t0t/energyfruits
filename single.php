2d5c044fab830ca45fdbbf590eead8ab7b271caf
Sergio Forés <sergiofores@gmail.com>
2015-09-30 13:18:42 +0200

    Editable button CTA :o2:

--- single.php
+++ single.php
@@ -1,12 +1,8 @@
 <?php get_header(); ?>
-
 <main class="main__content" role="main">
-
 		<?php
 		/* Content single */
 		get_template_part( 'partials/content', 'single' );
 		?>
-
 </main>
-
 <?php get_footer(); ?>
