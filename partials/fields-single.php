c4ac67f53b7bf8c701abc574f0c1cb0475452464
Sergio Forés <sergiofores@gmail.com>
2015-10-01 11:09:16 +0200

    Fixed bug in Recetas :cold_sweat:

--- partials/fields-single.php
+++ partials/fields-single.php
@@ -46,6 +46,38 @@ if (have_rows('layout_content')):
               /* The post content */
               the_sub_field("content");
               ?>
+              <?php
+              /* Recetas (lista) */
+              if (have_rows( 'lista_simple' )): ?>
+              <?php while (have_rows( 'lista_simple' )): the_row();?>
+                <ul class="list list--unordered">
+                <?php if (get_sub_field( "title" )): ?>
+                  <li><?php the_sub_field( 'title' ); ?></li>
+                <?php endif?>
+                <?php while (has_sub_field( 'lista_simple_elemento' )): ?>
+                <?php
+                  $txt = get_sub_field('txt');
+                  $link = get_sub_field('link');
+                ?>
+                  <li class="list__item">
+                    <?php if ( $link ): ?>
+                      <a href="<?php the_sub_field( 'link' ); ?>">
+                      <?php endif; ?>
+                      <?php if ( $txt ): ?>
+                      <?php the_sub_field( 'txt' );?>
+                      <?php if (get_sub_field( "external_url" )): ?>
+                      <?php the_sub_field( 'external_url' ); ?>
+                      <?php endif ?>
+                      <?php endif; ?>
+                      <?php if ( $link ): ?>
+                      </a>
+                    <?php endif; ?>
+                  </li>
+                <?php endwhile; ?>
+                </ul>
+              <?php endwhile; ?>
+              <?php endif; /* / Recetas (lista) */ ?>
+
           		<?php
               /* Facebook Comments */
               $var = do_shortcode('[fbcomments]'); echo $var;
