1c3948b4519168b1e97c9ad6cda756f14ee156af
Sergio Forés <sergiofores@gmail.com>
2015-10-05 12:19:55 +0200

    Fixed Archive bug

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php get_header(); ?>
-
 <main class="main__content" role="main">
-
 	<section>
-
 		<?php
 		/* Get content */
 		get_template_part( 'partials/content' ); ?>
-
 	</section>
-
 </main>
-
 <?php get_footer(); ?>
--- partials/content.php
+++ partials/content.php
@@ -2,7 +2,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<!-- Cargame los Custom fields, ACF -->
-	<?php get_template_part('templates/custom-fields'); ?>
+	<?php get_template_part('partials/custom-fields'); ?>
 
 <?php endwhile; ?>
 
